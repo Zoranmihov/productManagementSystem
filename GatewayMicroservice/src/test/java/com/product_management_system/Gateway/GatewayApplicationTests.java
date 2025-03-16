2debfb1c0314b819120d24e0cebdb96cfa215d9f
Zoran Mihov <mihov.zoran@hotmail.com>
2025-03-16 09:48:27 +0100

    Initial

---
+++ GatewayMicroservice/src/test/java/com/product_management_system/Gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product_management_system.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
